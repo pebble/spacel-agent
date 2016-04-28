bb3e23106c20738fe239cd5ba29b55f42d364584
Peter Wagner <pwagner@pebble.com>
2016-04-27 20:26:58 -0400

    Dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ requires = [
     'boto3==1.3.1',
     'botocore==1.4.13',
     'docutils==0.12',
+    'funcsigs==1.0.2',
     'futures==3.0.5',
     'jmespath==0.9.0',
-    'python-dateutil==2.5.3',
-    'python-systemd==0.1-planning-0'
-]
-
-dependency_links = [
-    'https://github.com/thepwagner/python-systemd/tarball/master#egg=0.1-planning-0'
+    'mock==2.0.0',
+    'pbr==1.9.1',
+    'python-dateutil==2.5.3'
 ]
 
 setup(name='spacel-agent',
@@ -21,7 +19,6 @@ setup(name='spacel-agent',
       url='https://github.com/mycloudandme/spacel-agent',
       author='Pete Wagner',
       author_email='github@mycloudand.me',
-      dependency_links=dependency_links,
       license='MIT',
       package_dir={'': 'src'},
       packages=find_packages('src', exclude=['test']),
