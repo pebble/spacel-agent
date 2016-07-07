fa728de269b04d6020c56be81061c2ebafb831dd
Chase Sillevis <chase@pebble.com>
2016-07-07 13:21:01 +0200

    Hotfix: use current folder

--- src/spacel/commands.py
+++ src/spacel/commands.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from .log import setup_logging
-from .cli import cli
+from log import setup_logging
+from cli import cli
 
 if __name__ == '__main__':  # pragma: no cover
     import logging
