e6fa0aa06348fef66328582e21888f4c7fcb773f
Peter Wagner <pwagner@pebble.com>
2016-09-06 19:37:29 -0400

    vz-spacel-agent build fix
    The git URL for python-systemd causes pip to explode, so don't list it
    there.
    Missed this detail when merging the setup.py and requirements.txt lists.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+requires = [req for req in open('requirements.txt').read().splitlines()
+            if not req.startswith('git')]
+
 setup(name='spacel-agent',
       version='0.0.1',
       description='Space Elevator agent',
@@ -10,5 +13,5 @@ setup(name='spacel-agent',
       license='MIT',
       package_dir={'': 'src'},
       packages=find_packages('src', exclude=['test']),
-      install_requires=open('requirements.txt').read().splitlines(),
+      install_requires=requires,
       zip_safe=True)
